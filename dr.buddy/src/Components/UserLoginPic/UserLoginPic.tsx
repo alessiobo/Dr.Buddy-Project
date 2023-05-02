571017dc46036583dc977b94b3de5d129ac90abd
Cipriano Mereu <ciprianomereu@gmail.com>
2023-05-02 18:59:12 +0200

    add:photo

--- dr.buddy/src/Components/UserLoginPic/UserLoginPic.tsx
+++ dr.buddy/src/Components/UserLoginPic/UserLoginPic.tsx
@@ -3,10 +3,16 @@ import "./userLoginPic.css";
 //@ts-ignore
 import defaultPic from "./circle-user.svg";
 
-function UserLoginPic({ name = "" }) {
+function UserLoginPic({
+  name = "",
+  personalImg,
+}: {
+  name: string;
+  personalImg?: any;
+}) {
   return (
     <div className="ulp-container">
-      <img src={defaultPic} alt="pic" />
+      <img src={personalImg ? personalImg : defaultPic} alt="pic" />
       <span className="ulp-online"></span>
       <h3 style={{ lineHeight: "inherit" }}>{name}</h3>
     </div>
