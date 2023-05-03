a99f1a993abf9806016081411df41810bb490546
marcobonni <marco.bonni19@gmail.com>
2023-05-03 15:07:49 +0200

    change colors and fix overflow

--- dr.buddy/src/Pages/LoginPage/RegisterPage.js
+++ dr.buddy/src/Pages/LoginPage/RegisterPage.js
@@ -4,7 +4,7 @@ import Navbar from "../../Components/Navbar/Navbar"
 import Register_Card from "../../Components/Register_Card/Register_Card"
 import "./RegisterPage.css"
 import dottoressa from "./dottoressa.svg"
-
+import dottoressa_no_cut from "./dottoressa_no_cut.svg"
 function LoginPage({createPatient}) {
     return (
         <div className="LoginPage">
@@ -12,7 +12,7 @@ function LoginPage({createPatient}) {
         <div className="body">
             <div className="body_element_container">
                 <Register_Card createPatient={createPatient} />
-                <img src={dottoressa}/>
+                <img src={dottoressa} className="dottoressa"/>
             </div>
         </div>
         <Footer/>
