9d95cabe34a97d7c28f0ccef9b34bccf9ff2fb94
Roberta <federicoroberta1@gmail.com>
2023-05-11 16:50:31 +0200

    part client card modificata

--- dr.buddy/src/Components/UserLoginPic/UserLoginPic.tsx
+++ dr.buddy/src/Components/UserLoginPic/UserLoginPic.tsx
@@ -2,6 +2,7 @@ import "./userLoginPic.css";
 
 //@ts-ignore
 import defaultPic from "./circle-user.svg";
+import TableContainer from "../../UI/Container/TableContainer/TableContainer";
 
 function UserLoginPic({
   name = "",
@@ -11,10 +12,18 @@ function UserLoginPic({
   personalImg?: any;
 }) {
   return (
-    <div className="ulp-container">
-      <img src={personalImg ? personalImg : defaultPic} alt="pic" />
-      <span className="ulp-online"></span>
-      <h3 style={{ lineHeight: "inherit" }}>{name}</h3>
+    <div className="doctorProfileTable-cont">
+      <TableContainer>
+        <h3>Profilo:</h3>
+
+        <div className="card-container">
+          <img className="round" src={defaultPic} alt="user" />
+          <h4>Pippo Esposito</h4>
+          <h4>Napoli</h4>
+          <p>Paziente</p>
+        </div>
+        <span></span>
+      </TableContainer>
     </div>
   );
 }
