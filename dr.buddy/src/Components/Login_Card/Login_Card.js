ff0f8f3c0b9e152914e55dc933e0747a76a1a77c
Cipriano Mereu <94009419+GitAbbonu@users.noreply.github.com>
2023-05-12 16:29:07 +0200

    Doctor page (#47)
    * add: UserLoginPic
    
    * add: json placeholder
    
    * add:scrollbar
    
    * fix pic from userLogin
    
    * fix:my css
    
    * add:Server
    
    * create:patients & reservations
    
    * add:useServer
    
    * add:to dev
    
    * fix:data_reservation
    
    * add:create reservation
    
    * fix tabella
    
    * add:state change
    
    * add:log in
    
    * add:log in
    
    * bug: logout
    
    * add:today reservations
    
    * add:today reservations
    
    * merge
    
    * add:half h
    
    * change color
    
    * change color
    
    * add:photo
    
    * add:photo
    
    * add:new reservations
    
    * create table doctor
    
    * create table doctor
    
    * fix:db
    
    * restart docto-page
    
    * restart docto-page
    
    * add:navbar and patients-table
    
    * start calendar
    
    * delete bootstrap
    
    * pull
    
    * add-button
    
    * add-button
    
    * fix:errors
    
    * add useReservation
    
    * merge develope and change calendar
    
    * fix border btn
    
    * start today reservations
    
    * add: today reservations
    
    * add: today reservations
    
    * add: doctor table profile
    
    * add: doctor table profile
    
    * add: doctor table profile
    
    * add Role and checkToken
    
    * add Role and checkToken
    
    * addio
    
    * start login
    
    * start login
    
    * login
    
    ---------
    
    Co-authored-by: Cipriano Mereu <ciprianomereu@gmail.com>

--- dr.buddy/src/Components/Login_Card/Login_Card.js
+++ dr.buddy/src/Components/Login_Card/Login_Card.js
@@ -1,34 +1,57 @@
-
-import "./LoginCard.css"
+import "./LoginCard.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-function Login_card() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    async function HandleData() {
-        const inputData = {
-            email: email,
-            password: password
-        }
-    }
-    return (
-        <div className="LoginCard">
-            <div class="shape"></div>
-        <form >
-            <h3>Accedi qui</h3>
+import Cookies from "js-cookie";
 
-            <label for="username">Email</label>
-            <input type="text" placeholder="Email" id="username" onChange={(event) => {setEmail(event.target.value)}} />
+function Login_card({ loginDoctor }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function HandleData(ev) {
+    ev.preventDefault();
+    const inputData = {
+      firstname: email, //cambiare email
+      password: password,
+    };
 
-            <label for="password">Password</label>
-            <input type="password" placeholder="Password" id="password" onChange={(event) => {setPassword(event.target.value)}}/>
-            <label>Prima volta su Dr.Buddy? <Link to="/register"><span className="clicca_qui">Clicca qui</span></Link></label>
-            <button onClick={HandleData}>Accedi</button>
-        </form>
-                <div class="shape"></div>
-        </div>
-        
-    
-    )
+    loginDoctor(inputData);
+
+    console.log(Cookies.get("token"));
+  }
+  return (
+    <div className="LoginCard">
+      <div class="shape"></div>
+      <form>
+        <h3>Accedi qui</h3>
+
+        <label for="username">Email</label>
+        <input
+          type="text"
+          placeholder="Email"
+          id="username"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+
+        <label for="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <label>
+          Prima volta su Dr.Buddy?{" "}
+          <Link to="/register">
+            <span className="clicca_qui">Clicca qui</span>
+          </Link>
+        </label>
+        <button onClick={HandleData}>Accedi</button>
+      </form>
+      <div class="shape"></div>
+    </div>
+  );
 }
-export default Login_card
+export default Login_card;
