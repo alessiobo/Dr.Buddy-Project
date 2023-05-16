71c9c1d2fceab10e2180456a8e3a5f5ec1f5792e
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-05-16 17:23:18 +0200

    fix

--- dr.buddy/src/Pages/Contacts/ContactsPage.tsx
+++ dr.buddy/src/Pages/Contacts/ContactsPage.tsx
@@ -4,24 +4,28 @@ import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import Cards from "./Cards";
 import gio from "./imgProfili/gio.jpg";
+import cip from "./imgProfili/Cip.jpg";
+import edo from "./imgProfili/Edo.jpg";
 
 function ContactsPage() {
-  return (
+  return (<>
+    <Navbar />
     <div className="Contacts">
-      <Navbar />
+      
       <div className="Container">
        
-   <Cards text1={<h2>Cipriano Mereu</h2>} text2={<p>FullStack Developer</p>} image="{gio}" />
+   <Cards text1={<h2>Cipriano Mereu</h2>} text2={<p>FullStack Developer</p>} image={cip} />
    <Cards text1={<h2>Giovanni Composto</h2>} text2={<p>FullStack Developer</p>}image={gio} />
    <Cards text1={<h2>Marco Bonnì</h2>} text2={<p>FullStack Developer</p>}image="{gio}" />
    <Cards text1={<h2>Alessio Boscaro</h2>} text2={<p>FullStack Developer</p>} image="{gio}" />
    <Cards text1={<h2>Roberta Federico</h2>} text2={<p>FullStack Developer</p>} image="{gio}" />
-   <Cards text1={<h2>Edoardo Gentile</h2>} text2={<p>FullStack Developer</p>} image="{gio}" />
+   <Cards text1={<h2>Edoardo Gentile</h2>} text2={<p>FullStack Developer</p>} image={edo} />
    </div>
      
-      <Footer/>
+      
        
     </div>
+    <Footer/></>
   );
 }
 
