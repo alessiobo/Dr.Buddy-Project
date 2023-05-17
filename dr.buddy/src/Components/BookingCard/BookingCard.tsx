7ed49e9ed3345966faaabad9a3380b1f3173c52d
Cipriano Mereu <ciprianomereu@gmail.com>
2023-05-17 20:45:40 +0200

    fix doctor prenotation

--- dr.buddy/src/Components/BookingCard/BookingCard.tsx
+++ dr.buddy/src/Components/BookingCard/BookingCard.tsx
@@ -53,34 +53,31 @@ function BookingCard({
     <div className="BookingCard">
       <div className="toggle-btn-wrapper">
         <div>
-          check prenotazioni
           <h1>qui tutte le prenotazione </h1>
           <button onClick={() => setDoctorchoice(1)}>Paperino</button>
           <button onClick={() => setDoctorchoice(2)}>Pluto</button>
           <div style={{ display: "flex", gap: "10px" }}>
-            {reservation.map((res: any, k) => {
-              return (
-                <div key={k}>
-                  {/* <CalendarBtn
-                    ora={getHour(res.date_reservation)}
-                    take={false}
-                  /> */}
-                  <h4>{res.date_reservation.substring(0, 10)}</h4>
-                  <button
-                    className="cal-btn-color"
-                    onClick={() =>
-                      updateReservation(res.id_reservation, {
-                        id_patient: 1,
-                        id_doctor: res.id_doctor,
-                        date_reservation: res.date_reservation,
-                      })
-                    }
-                  >
-                    {getHour(res.date_reservation)}
-                  </button>
-                </div>
-              );
-            })}
+            {reservation
+              .filter((res: any) => res.id_patient === null)
+              .map((res: any, k) => {
+                return (
+                  <div key={k}>
+                    <h4>{res.date_reservation.substring(0, 10)}</h4>
+                    <button
+                      className="cal-btn-color"
+                      onClick={() =>
+                        updateReservation(res.id_reservation, {
+                          id_patient: 1,
+                          id_doctor: res.id_doctor,
+                          date_reservation: res.date_reservation,
+                        })
+                      }
+                    >
+                      {getHour(res.date_reservation)}
+                    </button>
+                  </div>
+                );
+              })}
           </div>
         </div>
         {/* <button
