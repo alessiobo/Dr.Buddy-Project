9bddf100d29445d715cdea1910a83f022261a7d9
Cipriano Mereu <ciprianomereu@gmail.com>
2023-05-21 19:56:57 +0200

    fix

--- dr.buddy/src/Pages/RegisterPage/RegisterPage.js
+++ dr.buddy/src/Pages/RegisterPage/RegisterPage.js
@@ -1,12 +1,9 @@
-import Navbar from "../../Components/Navbar/Navbar";
 import RegisterCard from "../../Components/Register_Card/RegisterCard";
 import Footer from "../../Components/Footer/Footer";
 import "./RegisterPage.css";
 function LoginPage({ createPatient }) {
   return (
     <div className="RegisterPage">
-      {/* <Navbar/> */}
-
       <RegisterCard createPatient={createPatient} />
 
       <Footer />
